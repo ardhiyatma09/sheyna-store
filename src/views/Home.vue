d36bf0e8110d1d519f93f12275a14c11d6b28714
Ardhiyatma Raditya Wibawa <ardiyatma.wibawa12@gmail.com>
2020-09-02 13:29:34 +0700

    vue front end page

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header/>
+    <CarouselHero />
+    <WomenBanner/>
+    <Instagram/>
+    <Partner/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import CarouselHero from "@/components/CarouselHero.vue";
+import WomenBanner from "@/components/WomenBanner.vue";
+import Instagram from "@/components/Instagram.vue";
+import Partner from "@/components/Partner.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    CarouselHero,
+    WomenBanner,
+    Instagram,
+    Partner,
+    Footer
+  },
+};
+</script>
