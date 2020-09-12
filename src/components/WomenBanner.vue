52cf1621444a6cb1eda691dde1a9b7e8cc60522f
Ardhiyatma Raditya Wibawa <ardiyatma.wibawa12@gmail.com>
2020-09-12 12:39:13 +0700

    get data products from API backend shayna

--- src/components/WomenBanner.vue
+++ src/components/WomenBanner.vue
@@ -3,11 +3,13 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
                     <carousel class="product-slider" :nav="false" :autoplay="true" :dots="false">
-                        <div class="product-item">
+
+                        <div class="product-item" v-for="product in products" :key="product.id">
                             <div class="pi-pic">
-                                <img src="img/mickey1.jpg" alt="" />
+                                <img v-if="product.galleries[0]" :src="product.galleries[0].photo" alt="" />
+                                <img src="https://via.placeholder.com/500x500.png" alt="" v-else>
                                 <ul>
                                     <li class="w-icon active">
                                         <a href="#"><i class="icon_bag_alt"></i></a>
@@ -16,80 +18,22 @@
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
+                                <div class="catagory-name">{{product.type}}</div>
                                 <router-link to="/product">
-                                    <h5>Mickey Baggy</h5>
+                                    <h5>{{product.name}}</h5>
                                 </router-link>
                                 <div class="product-price">
-                                    $14.00
-                                    <span>$35.00</span>
+                                    ${{product.price}}
+                                    <!-- <span>$35.00</span> -->
                                 </div>
                             </div>
                         </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
+
                     </carousel>
+                </div>
+
+                <div class="col-lg-12" v-else>
+                    <p>data tidak di temukan</p>
                 </div>
             </div>
         </div>
@@ -99,12 +43,24 @@
 
 <script>
 import carousel from 'vue-owl-carousel'
+import axios from 'axios'
 
 export default {
     name: 'WomenBanner',
     components: {
         carousel,
   },
+  data() {
+      return{
+          products: []
+      }
+  },
+  mounted() {
+      axios
+        .get("http://127.0.0.1:8000/api/products")
+        .then(res => (this.products = res.data.data.data))
+        .catch(err => console.log(err));
+  }
 }
 </script>
 
